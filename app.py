cfed137cb7315705b32a4c0a8a4eefd50ea14396
Nat Burgwyn <burgwyn@gmail.com>
2019-03-20 20:42:47 -0400

    🐛 JSON 🐛

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    return Response("{ message: 'Hello World!' }", mimetype="application/json") 
+    return Response('{ "message": "Hello World!" }', mimetype="application/json") 
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
